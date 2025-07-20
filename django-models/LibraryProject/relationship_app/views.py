a68cb29f691ebb262e320e383427e683a6ce46e1
Aguigbo emmanuel <aguigbochidera@gmail.com>
2025-07-20 21:24:16 +0100

    checker error corrected

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,22 @@
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect
 from django.views.generic import DetailView
 from .models import Book, Library
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Or wherever you want to go after login
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
 
 # Function-based view to list all books
 def list_books(request):
