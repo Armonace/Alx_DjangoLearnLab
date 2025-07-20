bfef6ec7fbef7ee25c06c6857e518336011b4273
Aguigbo emmanuel <aguigbochidera@gmail.com>
2025-07-20 15:58:57 +0100

    admin page success

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  # Columns to show in admin list
+    list_filter = ('author', 'publication_year')            # Filters in the sidebar
+    search_fields = ('title', 'author')                     # Search bar fields
+
+
+admin.site.register(Book, BookAdmin)
