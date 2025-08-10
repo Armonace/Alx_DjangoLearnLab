8455988199f4ddc33f3ba20a761e4d64a61df1f7
Aguigbo emmanuel <aguigbochidera@gmail.com>
2025-08-10 18:18:29 +0100

    imported APITestCase

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,49 +1,47 @@
-from django.test import TestCase
-from rest_framework.test import APIClient
+from rest_framework.test import APITestCase
 from rest_framework import status
-from .models import Author, Book
+from .models import Book
 
-class BookAPITests(TestCase):
+class BookAPITests(APITestCase):
+
     def setUp(self):
-        self.client = APIClient()
-        self.author = Author.objects.create(name='John Doe')
         self.book = Book.objects.create(
-            title='Test Book',
-            description='Test Description',
-            author=self.author
+            title="Test Book",
+            author="John Doe",
+            description="A sample description",
+            published_date="2025-01-01"
         )
-
-    def test_list_books(self):
-        response = self.client.get('/api/books/')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertIsInstance(response.data, list)
-
-    def test_retrieve_book(self):
-        response = self.client.get(f'/api/books/{self.book.id}/')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['title'], self.book.title)
 
     def test_create_book(self):
         data = {
             "title": "New Book",
-            "description": "A new book description",
-            "author": self.author.id
+            "author": "Jane Doe",
+            "description": "Another description",
+            "published_date": "2025-02-01"
         }
-        response = self.client.post('/api/books/create/', data)
+        response = self.client.post('/api/books/create/', data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(response.data['title'], "New Book")
+        self.assertEqual(Book.objects.count(), 2)
+
+    def test_get_book_list(self):
+        response = self.client.get('/api/books/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertGreaterEqual(len(response.data), 1)
 
     def test_update_book(self):
         data = {
             "title": "Updated Book",
+            "author": "John Doe",
             "description": "Updated description",
-            "author": self.author.id
+            "published_date": "2025-01-01"
         }
         response = self.client.put(f'/api/books/{self.book.id}/update/', data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['title'], "Updated Book")
+        self.book.refresh_from_db()
+        self.assertEqual(self.book.title, "Updated Book")
 
     def test_delete_book(self):
         response = self.client.delete(f'/api/books/{self.book.id}/delete/')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertFalse(Book.objects.filter(id=self.book.id).exists())
+        self.assertEqual(Book.objects.count(), 0)
+
