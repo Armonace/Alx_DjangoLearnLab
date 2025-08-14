0a374fd7e0bf34d6337b971dbc59e0cc9bfdaad2
Aguigbo emmanuel <aguigbochidera@gmail.com>
2025-08-14 12:12:14 +0100

    added user to settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -77,6 +77,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'mydbuser',
         'HOST': 'localhost',          # or an IP address
         'PORT': '5432',    
     }
